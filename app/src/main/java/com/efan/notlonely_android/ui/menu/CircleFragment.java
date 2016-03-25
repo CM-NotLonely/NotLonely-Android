69ca2288f8c8795df00c17788608a524ea07a061
linqh0806 <359661501@qq.com>
2016-03-25 21:29:55 +0800

    today task finish

--- app/src/main/java/com/efan/notlonely_android/ui/menu/CircleFragment.java
+++ app/src/main/java/com/efan/notlonely_android/ui/menu/CircleFragment.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.efan.basecmlib.activity.BaseFragment;
+import com.efan.basecmlib.annotate.ViewInject;
 import com.efan.notlonely_android.R;
+import com.efan.notlonely_android.view.BlurringView;
 
 /**
  * Created by linqh0806 on 16-3-23.
  */
 public class CircleFragment extends BaseFragment{
+    @ViewInject(id=R.id.blur)
+    private BlurringView blurringView;
+    @ViewInject(id=R.id.iv_image)
+    private ImageView iv;
 
     @Override
     protected View inflaterView(LayoutInflater var1, ViewGroup var2, Bundle var3) {
@@ -20,7 +27,7 @@ public class CircleFragment extends BaseFragment{
 
     @Override
     public void initData() {
-
+        blurringView.setBlurredView(iv);
     }
 
     @Override
