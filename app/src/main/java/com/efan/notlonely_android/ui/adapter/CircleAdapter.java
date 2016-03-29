e499b45338d198816e1576dc570a65bbf835d5de
linqh0806 <359661501@qq.com>
2016-03-29 21:26:47 +0800

    拉取

--- app/src/main/java/com/efan/notlonely_android/ui/adapter/CircleAdapter.java
+++ app/src/main/java/com/efan/notlonely_android/ui/adapter/CircleAdapter.java
@@ -1,14 +1,21 @@
 package com.efan.notlonely_android.ui.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Message;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.efan.notlonely_android.R;
+import com.efan.notlonely_android.utils.blurry.Blurry;
 import com.efan.notlonely_android.view.BlurringView;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by linqh0806 on 16-3-25.
@@ -19,9 +26,10 @@ public class CircleAdapter extends RecyclerView.Adapter<CircleAdapter.vHold> {
     private static final int TYPE_FOOTER = 1;
     private Context mContext;
     private LayoutInflater mInflater;
-
+    private ArrayList<Drawable> mData;
 
     private onItemClickListener onItemClickListener;
+
 
     public interface onItemClickListener {
         void onItemClick(View view, int position);
@@ -33,21 +41,22 @@ public class CircleAdapter extends RecyclerView.Adapter<CircleAdapter.vHold> {
         this.onItemClickListener = onItemClickListener;
     }
 
-    public CircleAdapter(Context mContext) {
+    public CircleAdapter(Context mContext,ArrayList<Drawable> mData) {
         this.mContext = mContext;
+        this.mData=mData;
         mInflater = LayoutInflater.from(mContext);
     }
 
     @Override
     public vHold onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.item_cardview_circle, parent, false);
-        vHold hold = new vHold(view);
-        return hold;
+        vHold holder = new vHold(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(final vHold holder, final int position) {
-        holder.blurringView.setBlurredView(holder.imageView);
+        holder.imageView.setImageDrawable(mData.get(position));
         if (onItemClickListener != null) {
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -65,19 +74,20 @@ public class CircleAdapter extends RecyclerView.Adapter<CircleAdapter.vHold> {
         }
     }
 
+
     @Override
     public int getItemCount() {
-        return 5;
+        return mData.size();
     }
 
     public class vHold extends RecyclerView.ViewHolder {
-        BlurringView blurringView;
-        ImageView imageView;
+        public ImageView imageView;
+        public BlurringView blurringView;
         public vHold(View itemView) {
             super(itemView);
-            blurringView= (BlurringView) itemView.findViewById(R.id.blur);
-            imageView= (ImageView) itemView.findViewById(R.id.iv_background);
-            blurringView.setBlurredView(imageView);
+            imageView = (ImageView) itemView.findViewById(R.id.iv_background);
+            blurringView= (BlurringView) itemView.findViewById(R.id.blurring);
         }
     }
+
 }
--- app/src/main/java/com/efan/notlonely_android/ui/menu/CircleFragment.java
+++ app/src/main/java/com/efan/notlonely_android/ui/menu/CircleFragment.java
@@ -1,5 +1,8 @@
 package com.efan.notlonely_android.ui.menu;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,45 +15,101 @@ import com.efan.basecmlib.activity.BaseFragment;
 import com.efan.basecmlib.annotate.ViewInject;
 import com.efan.notlonely_android.R;
 import com.efan.notlonely_android.ui.adapter.CircleAdapter;
+import com.efan.notlonely_android.utils.ImageUtils;
+import com.efan.notlonely_android.utils.blurry.Blurry;
 import com.efan.notlonely_android.view.BlurringView;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by linqh0806 on 16-3-23.
  */
-public class CircleFragment extends BaseFragment{
-    @ViewInject(id=R.id.blur)
-    private BlurringView blur;
-    @ViewInject(id=R.id.recyclerview)
+public class CircleFragment extends BaseFragment {
+    @ViewInject(id = R.id.recyclerview)
     private RecyclerView recyclerView;
 
     private CircleAdapter adapter;
     private BlurringView blurringView;
+    private LinearLayoutManager layoutManager;
+    private ArrayList<Drawable> mData;
+
     @Override
     protected View inflaterView(LayoutInflater var1, ViewGroup var2, Bundle var3) {
-        return var1.inflate(R.layout.fragment_circle,var2,false);
+        return var1.inflate(R.layout.fragment_circle, var2, false);
     }
 
     @Override
     public void initView() {
-        blurringView= (BlurringView) getActivity().findViewById(R.id.blurring_view);
+        Fresco.initialize(getContext());
+        blurringView = (BlurringView) getActivity().findViewById(R.id.blurring_view);
     }
 
     @Override
     public void initData() {
-        adapter=new CircleAdapter(getContext());
+        mData = new ArrayList<Drawable>();
+        {
+            Bitmap bitmap = ImageUtils.drawableToBitmap(getResources().getDrawable(R.mipmap.test));
+            Bitmap blurBitmap = ImageUtils.doBlur(bitmap, 30, true);   //bitmap to blur bitmap
+            Drawable blurdrawable = ImageUtils.BitmapToDrawble(blurBitmap);
+            mData.add(blurdrawable);
+        }
+        {
+            Bitmap bitmap = ImageUtils.drawableToBitmap(getResources().getDrawable(R.mipmap.test1));
+            Bitmap blurBitmap = ImageUtils.doBlur(bitmap, 30, true);   //bitmap to blur bitmap
+            Drawable blurdrawable = ImageUtils.BitmapToDrawble(blurBitmap);
+            mData.add(blurdrawable);
+        }
+        adapter = new CircleAdapter(getContext(),mData);
         recyclerView.setAdapter(adapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
     }
 
     @Override
     public void initEvent() {
+        adapter.setOnItemClickListener(new CircleAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Blurry.with(getContext())
+                        .radius(5)
+                        .sampling(2)
+                        .async()
+                        .capture(view.findViewById(R.id.iv_background))
+                        .into((ImageView) view.findViewById(R.id.iv_background));
+            }
 
+            @Override
+            public void onItemLongClick(View view, int position) {
+                initBlur();
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+
+    /**
+     * 对RecyclerView中的item图片进行高斯模糊处理
+     */
+    private void initBlur() {
+        for (int i = 0; i < adapter.getItemCount(); i++) {
+            View views = recyclerView.getChildAt(i);
+            if (views != null) {
+                CircleAdapter.vHold vHold = (CircleAdapter.vHold) recyclerView.getChildViewHolder(views);
+                View blurringView = vHold.imageView;
+                Blurry.with(getContext())
+                        .radius(5)
+                        .sampling(2)
+                        .async()
+                        .capture(blurringView)
+                        .into((ImageView) blurringView);
+            }
+        }
     }
 
     @Override
@@ -62,6 +121,8 @@ public class CircleFragment extends BaseFragment{
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
-        if(!hidden&&blurringView instanceof BlurringView) blurringView.invalidate();
+        if (!hidden && blurringView instanceof BlurringView) {
+            blurringView.invalidate();
+        }
     }
 }
